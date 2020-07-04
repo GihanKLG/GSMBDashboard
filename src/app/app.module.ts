92541ac865992b1978d3930275d352f2333d19a8
GihanDarshana <45061133+GihanDarshana@users.noreply.github.com>
2020-07-04 21:09:32 +0530

    add admin dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
+import { GooglemapComponent } from './googlemap/googlemap.component';
+
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'googlemap', component: GooglemapComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    GooglemapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
